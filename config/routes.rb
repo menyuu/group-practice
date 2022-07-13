090b33728ef00b04ff79781ad71ed0f51c9c08f7
menyuu <101458830+menyuu@users.noreply.github.com>
2022-07-13 15:53:36 +0000

    user, tagのコントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'tag/index'
+  get 'tag/new'
+  get 'tag/edit'
+  get 'user/edit'
   devise_for :users
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
---
+++ test/controllers/tag_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class TagControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get tag_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get tag_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get tag_edit_url
+    assert_response :success
+  end
+end
