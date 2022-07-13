c9dacc54347f841ea41d9564f8ea79e3748a740a
menyuu <101458830+menyuu@users.noreply.github.com>
2022-07-13 16:20:36 +0000

    user, tagコントローラの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'tag/index'
-  get 'tag/new'
-  get 'tag/edit'
-  get 'user/edit'
   devise_for :users
+  resources :tags, only: [:index, :new, :edit, :create, :update, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
