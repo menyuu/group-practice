045766e58ca5242aeb8364575eb35d1a2d764b88
menyuu <101458830+menyuu@users.noreply.github.com>
2022-07-15 11:02:45 +0000

    ユーザーのtag機能の追加

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,28 @@
 class TagsController < ApplicationController
   def index
+    @tags = Tag.all
+    @tag = Tag.new
   end
 
-  def new
+  def create
+    tag = Tag.find_or_create_by(tag_params)
+    if current_user.tag_users.find_by(tag_id: tag.id)
+      return redirect_to users_path
+    else
+      tag_user = TagUser.new
+      tag_user.tag_id = tag.id
+      tag_user.user_id = current_user
+      current_user.tags.push(tag)
+      redirect_to tags_path
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     post 'users/sign_up/confirm', to: 'users/registrations#confirm'
     get 'users/sign_up/complete', to: 'users/registrations#complete'
   end
-  resources :tags, only: [:index, :new, :edit, :create, :update, :destroy]
+  resources :tags, only: [:index, :edit, :create, :update, :destroy]
   resources :users, only: [:index, :show, :edit, :update]
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
