d0ec009c62d86412a1f1440bfcc0a260ff5d88a2
menyuu <101458830+menyuu@users.noreply.github.com>
2022-07-16 14:38:46 +0000

    tag機能の追加

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,26 +1,12 @@
 class TagsController < ApplicationController
   def index
+    @user = current_user
     @tags = Tag.all
     tags = current_user.tags.all
     @user_tags = tags.pluck(:name).join(",")
   end
 
   def create
-    tags = params[:tag][:name].split(',')
-    current_tags = current_user.tags.pluck(:name) unless current_user.tags.nil?
-    old_tags = current_tags - tags
-    new_tags = tags - current_tags
-    old_tags.each do |old|
-      tags.delete Tag.find_by(name: old)
-    end
-    new_tags.each do |new|
-      tag = Tag.find_or_create_by(name: new)
-      current_user.tags << tag
-    end
-    redirect_to tags_path
-  end
-
-  def edit
   end
 
   private
