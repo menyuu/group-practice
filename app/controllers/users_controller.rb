80402f018ac126eb46644efb9b7859ccab260817
menyuu <101458830+menyuu@users.noreply.github.com>
2022-07-16 18:35:31 +0000

    [Update]タグ機能の更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,37 @@
 class UsersController < ApplicationController
-  def edit
+  def index
+    @user = User.all
   end
 
-  def update
-    tags = params[:user][:name].split(',')
-    current_tags = current_user.tags.pluck(:name) unless current_user.tags.nil?
-    old_tags = current_tags - tags
-    new_tags = tags - current_tags
-    old_tags.each do |old|
-      current_user.tags.delete Tag.find_by(name: old)
-    end
-    new_tags.each do |new|
-      tag = Tag.find_or_create_by(name: new)
+  def user_tag_create
+    tags = params[:tag][:name].split(',')
+    tags.each do |tag|
+      tag = Tag.find_or_create_by(name: tag)
       current_user.tags << tag
     end
     redirect_to tags_path
   end
 
+  def edit
+  end
+
+  def user_tag_update
+    if params[:user][:tag_ids]
+      get_tags = Tag.where(id: params[:user][:tag_ids])
+      tags = []
+      get_tags.each do |tag|
+        tags.push(tag.name)
+      end
+      current_user.tag_save(tags)
+      redirect_to tags_path
+    else
+      redirect_to tags_path
+    end
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, tag_ids: [])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,10 @@ Rails.application.routes.draw do
     get 'users/sign_up/complete', to: 'users/registrations#complete'
   end
   resources :tags, only: [:index, :edit, :create, :update, :destroy]
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    post "tags/create" => "users#user_tag_create", on: :collection
+    patch "tags/update" => "users#user_tag_update"
+  end
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
